1e64d5ef9c51b59c7f749209eec21ef945f7fa69
S-SMTN <cven28@gmail.com>
2024-05-07 18:03:01 +0300

    added implemented home and login page with base and sidebar templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from app.models import Position, Worker, TaskType, Task
+
+
 # Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+
+    num_positions = Position.objects.count()
+    num_workers = Worker.objects.count()
+    num_task_types = TaskType.objects.count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num_positions": num_positions,
+        "num_workers": num_workers,
+        "num_task_types": num_task_types,
+        "num_tasks": num_tasks
+    }
+
+    return render(request, "app/index.html", context=context)

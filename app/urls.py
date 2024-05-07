ce2b901ac65c4c26f1bc28075dbca1bbd5aebc22
S-SMTN <cven28@gmail.com>
2024-05-07 21:28:07 +0300

    implemented add/update/delete/search position

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 
-from app.views import index, PositionListView
+from app.views import index, PositionListView, PositionCreateView, PositionUpdateView, PositionDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("positions", PositionListView.as_view(), name="position-list")
+    path("positions", PositionListView.as_view(), name="position-list"),
+    path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create",
+    ),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "position/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
 ]
 
 app_name = "app"
